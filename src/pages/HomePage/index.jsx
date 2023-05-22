c2ab11cb6f39bf1aa72412f02c34244cc360b1ac
Vy <motchiecvee@Little-Vee.local>
2023-05-22 11:35:30 +0700

    fix homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return <Navigate to="/user-management" />;
 }
 
 export default HomePage;
